ac5bff1b72742c4056aaa913c2d1cd348f845e2a
Muhammad Talha <talha18513@gmail.com>
2025-06-14 06:54:23 +0500

    first

--- docs/languages/de/javascript/footer.js
+++ docs/languages/de/javascript/footer.js
@@ -6,23 +6,23 @@ footer.innerHTML = `
             <img src="/docs/assets/images/white-logo.svg" class="h-5 invert " alt="">
             <div class="flex md:items-center md:justify-center mt-5 md:mt-0 md:gap-16 gap-5 md:flex-row flex-col">
                 <div class="">
-                    <h2 class="text-white uppercase font-medium text-xl">Contact</h2>
+                    <h2 class="text-white uppercase font-medium text-xl">Kontakt</h2>
                     <a href="mailto:info@dgrgroup.net"
                         class="hover:underline text-zinc-200 mt-1 block text-lg">info@dgrgroup.net</a>
                 </div>
                 <div class="">
-                    <h2 class="text-white uppercase font-medium text-xl">Press & Public Relations</h2>
+                    <h2 class="text-white uppercase font-medium text-xl">Presse- und Öffentlichkeitsarbeit</h2>
                     <a href="mailto:info@dgrgroup.net"
                         class="hover:underline text-zinc-200 mt-1 block text-lg">info@dgrgroup.net</a>
                 </div>
             </div>
         </div>
         <div class="flex items-center max-w-7xl w-full mx-auto py-8 justify-between gap-5 md:flex-row flex-col">
-            <p class="text-zinc-200">Copyright © <span id="copyright"></span> DGR Consult Invest GmbH All rights reserved</p>
+            <p class="text-zinc-200">Copyright © <span id="copyright"></span> DGR Consult Invest GmbH Alle Rechte vorbehalten</p>
             <div class="flex items-center gap-5">
-                <a href="/privacy-policy.html" class="hover:underline text-zinc-200 block ">Privacy Policy</a>
-                <a href="/imprint.html" class="hover:underline text-zinc-200 block ">Imprint</a>
-                <a href="/careers.html" class="hover:underline text-zinc-200 block ">Careers</a>
+                <a href="/docs/languages/de/privacy-policy.html" class="hover:underline text-zinc-200 block ">Datenschutzrichtlinie</a>
+                <a href="/docs/languages/de/imprint.html" class="hover:underline text-zinc-200 block ">Impressum</a>
+                <a href="/docs/languages/de/careers.html" class="hover:underline text-zinc-200 block ">Karriere</a>
             </div>
         </div>
     </footer>
--- docs/languages/de/javascript/navbar.js
+++ docs/languages/de/javascript/navbar.js
@@ -4,39 +4,37 @@ dynamic_navbar.innerHTML = `
     <nav class="fixed navbar md:top-6 top-4 px-4 left-0 w-full z-40 transition-all duration-300">
         <div data-aos="fade-down"
             class="max-w-7xl mx-auto glass rounded-2xl flex justify-between items-center w-full md:p-5 p-3 md:h-28 h-20">
-            <a href="/index.html" class="flex items-center justify-center lg:justify-start">
+            <a href="/docs/languages/de/index.html" class="flex items-center justify-center lg:justify-start">
                 <img src="/docs/assets/images/logo.png" class="md:h-20 h-16" alt="logo">
             </a>
             <div class="md:flex hidden items-center gap-10">
                 <div class="md:flex items-center gap-10 hidden">
                     <ul class="md:flex hidden items-center text-zinc-800 font-medium uppercase gap-10">
-                        <li><a href="/index.html" class="hover:text-opacity-80 transition-all duration-500">Home </a>
+                        <li><a href="/docs/languages/de/index.html" class="hover:text-opacity-80 transition-all duration-500">Heim </a>
                         </li>
-                        <li><a href="/scope.html" class="hover:text-opacity-80 transition-all duration-500">Scope</a>
+                        <li><a href="/docs/languages/de/scope.html" class="hover:text-opacity-80 transition-all duration-500">Umfang</a>
                         </li>
-                        <li><a href="/philosophy.html" class="hover:text-opacity-80 transition-all duration-500">Philosophy</a>
+                        <li><a href="/docs/languages/de/philosophy.html" class="hover:text-opacity-80 transition-all duration-500">Philosophie</a>
                         </li>
-                        <li><a href="/industries.html" class="hover:text-opacity-80 transition-all duration-500">Industries</a>
+                        <li><a href="/docs/languages/de/industries.html" class="hover:text-opacity-80 transition-all duration-500">Branchen</a>
                         </li>
                     </ul>
                 </div>
             </div>
             <div class="md:flex items-center justify-center gap-4 hidden">
-                <a href="/careers.html"
-                    class="md:block hidden transition-all duration-500 hover:opacity-80 px-6 py-1.5 uppercase rounded-lg text-white bg-[#183385]">Careers</a>
+                <a href="/docs/languages/de/careers.html"
+                    class="md:block hidden transition-all duration-500 hover:opacity-80 px-6 py-1.5 uppercase rounded-lg text-white bg-[#183385]">Karriere</a>
                 <div class="w-0.5 bg-zinc-500 rounded-full h-6"></div>
                 <div class="flex items-center justify-center gap-2 relative">
                     <div id="language" class="cursor-pointer flex items-center gap-1">
                         <img src="/docs/assets/images/global.svg" class="opacity-80 h-6" alt="">
-                        <span class="font-medium uppercase">EN</span>
+                        <span class="font-medium uppercase">DE</span>
                     </div>
 
                     <div id="langDropdown" class="absolute fadeIn top-14 w-20 -left-2 glass p-3 rounded-xl shadow-md hidden">
                         <ul class="space-y-3">
-<li class="cursor-pointer hover:opacity-60 transition-all duration-500"><a href="/docs/languages/de/index.html"
-        class="block w-full">DE</a></li>
-<li class="cursor-pointer hover:opacity-60 transition-all duration-500"><a href="/index.html"
-        class="block w-full">EN</a></li>
+    <li class="cursor-pointer hover:opacity-60 transition-all duration-500"><a href="/index.html" class="block w-full">EN</a></li>
+    <li class="cursor-pointer hover:opacity-60 transition-all duration-500"><a href="/docs/languages/de/index.html" class="block w-full">DE</a></li>
                         </ul>
                     </div>
                 </div>
@@ -53,13 +51,13 @@ dynamic_navbar.innerHTML = `
         </div>
         <div class="p-6">
             <ul class="flex relative z-50 text-zinc-800 font-medium uppercase flex-col gap-7">
-              <li><a href="/index.html" class="hover:text-opacity-80 transition-all duration-500">Home </a>
+              <li><a href="/docs/languages/de/index.html" class="hover:text-opacity-80 transition-all duration-500">Heim </a>
                         </li>
-                        <li><a href="/scope.html" class="hover:text-opacity-80 transition-all duration-500">Scope</a>
+                        <li><a href="/docs/languages/de/scope.html" class="hover:text-opacity-80 transition-all duration-500">Umfang</a>
                         </li>
-                        <li><a href="/philosophy.html" class="hover:text-opacity-80 transition-all duration-500">Philosophy</a>
+                        <li><a href="/docs/languages/de/philosophy.html" class="hover:text-opacity-80 transition-all duration-500">Philosophie</a>
                         </li>
-                        <li><a href="/industries.html" class="hover:text-opacity-80 transition-all duration-500">Industries</a>
+                        <li><a href="/docs/languages/de/industries.html" class="hover:text-opacity-80 transition-all duration-500">Branchen</a>
                         </li>
             </ul>
             <ul
@@ -72,8 +70,8 @@ dynamic_navbar.innerHTML = `
                         class="hover:text-opacity-80 overflow-hidden transition-all duration-500 border rounded-lg w-full block py-1">DE</a>
                 </li>
             </ul>
-            <a href="/careers.html"
-                class=" block text-nowrap transition-all duration-500 hover:opacity-80 px-6 py-3 uppercase rounded-lg text-white bg-[#183385] text-center mt-3">Careers</a>
+            <a href="/docs/languages/de/careers.html"
+                class=" block text-nowrap transition-all duration-500 hover:opacity-80 px-6 py-3 uppercase rounded-lg text-white bg-[#183385] text-center mt-3">Karriere</a>
         </div>
     </div>
 `
